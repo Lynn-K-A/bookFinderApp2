f7959090f99cb23318e8f3abdb6e3437dac1715b
lynn@aspire1 <lynn@aspire1>
2022-04-04 19:03:34 +0200

    some changes and added css

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
+const list = document.getElementById("resultsList");
+const url = 'https://www.googleapis.com/books/v1/volumes';
+const queryParam = '?q=';
+const apiKey = 'AIzaSyDHCVvn21hvUh7qg-UF6bl5k9rW4xOVpys';
+const radios = document.getElementsByName("search-choice");
+const selected = Array.from(radios).find(radio => radio.checked);
+const lookFor = `+${selected.value}&key=`;
+
+
+
+
 const searchBook = (query) => {
-  const url = 'https://www.googleapis.com/books/v1/volumes';
-  const queryParam = '?q=';
-  const apiKey = 'AIzaSyDHCVvn21hvUh7qg-UF6bl5k9rW4xOVpys';
-  const lookFor = '+intitle&key='
   query = query.replace(' ','%20');
   const endpoint = `${url}${queryParam}${query}${lookFor}${apiKey}`; 
-  //console.log(endpoint);
+  console.log(endpoint);
 
   fetch(endpoint)
   .then(async response => {
@@ -16,7 +23,7 @@ const searchBook = (query) => {
         return jsonResponse;
       } throw new Error('Request failed!');
     } catch(error) {
-      console.log(`Oops.. something went wrong! ${error}`);
+      console.log(`Oops.. something went wrong! check the endpoint at ${error}`);
     }
   })
   .then(jsonData => {
@@ -24,49 +31,48 @@ const searchBook = (query) => {
     renderResults(results);
   })
   .catch(error => {
-    console.log(error);
+    console.log(`Render Error: ${error}`);
   });
 }
 
 const renderResults = (results) => {
-  const list = document.getElementById("resultsList");
   list.innerHTML = "";
   results.forEach(result => {
-    const element = document.createElement("li");
-    element.classList.add("list-element");
-    list.appendChild(element);
-    const bookName = document.createElement("h4")
-    element.appendChild(bookName);
-    bookName.textContent = result.volumeInfo.title;
-    const img = document.createElement('img');
-    img.classList.add("list-img");
-    img.src = result.volumeInfo.imageLinks.thumbnail;
-    img.alt = result.volumeInfo.title;
-    element.appendChild(img);
-    const showMoreBtn = document.createElement("button");
-    showMoreBtn.type = "button";
-    showMoreBtn.textContent = "Show More";
-    element.appendChild(showMoreBtn);
-    showMoreBtn.addEventListener('click',showInfo(result));
+    makeCards(result);
   });
 }
-
-const showInfo = (result) => {
-  const infoBox = document.createElement("div");
-      infoBox.classList.add("info-box");
-      document.body.appendChild(infoBox);
-      infoBox.style.visibility = 'visible';
-      const descriptionBox = document.createElement("div");
-      descriptionBox.classList.add("description-box");
-      infoBox.appendChild(descriptionBox);
-      descriptionBox.textContent = result.volumeInfo.description;
-      const bookTitle = document.createElement("h3");
-      bookTitle.classList.add("book-title");
-      infoBox.appendChild(bookTitle);
-      bookTitle.textContent = result.volumeInfo.title;
+ 
+const makeCards = (result) => {
+  const element = document.createElement("li");
+  element.classList.add("list-element");
+  list.appendChild(element);
+  const bookDiv = document.createElement("div")
+  element.appendChild(bookDiv);
+  bookDiv.classList.add("book-div");
+  const img = document.createElement("img");
+  img.classList.add("list-img");
+  img.src = result.volumeInfo.imageLinks.thumbnail;
+  img.alt = result.volumeInfo.title;
+  bookDiv.appendChild(img);
+  const bookInfoDiv = document.createElement("div")
+  bookDiv.appendChild(bookInfoDiv);
+  bookInfoDiv.classList.add("book-info-div");
+  const bookName = document.createElement("h3")
+  bookInfoDiv.appendChild(bookName);
+  bookName.textContent = result.volumeInfo.title;
+  const authorName = document.createElement("h4")
+  bookInfoDiv.appendChild(authorName);
+  authorName.textContent = `Author/Authors: ${result.volumeInfo.authors}`;
+  const showMoreBtn = document.createElement("button");
+  showMoreBtn.type = "button";
+  showMoreBtn.classList.add("show-more-btn")
+  showMoreBtn.textContent = "Show More";
+  bookInfoDiv.appendChild(showMoreBtn);
+  showMoreBtn.addEventListener('click', () => {
+    const redirect = result.volumeInfo.infoLink;
+    window.open(redirect, '_blank');
+  });
 }
-//const redirect = result.volumeInfo.infoLink;
-//window.open(redirect, '_blank');
 
 window.onload = () => {
   const searchFieldElement = document.getElementById('search');
